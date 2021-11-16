65a9ca0947577cbfefd0c317c731b71534d311a2
faheemjailor123 <93036039+faheemjailor123@users.noreply.github.com>
2021-11-16 22:50:19 +0530

    last name

---
+++ Last_name.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Regex_Last_Name_UC2
+{
+    class check  //create one class.
+    {
+        public static void firstName(string name)         //declaring the static meythod to take user input.
+        {
+            string first = "^[A-Z]{1}[a-z]{2,}$";        //write regularExpration.   ^=start  $=end.
+            Regex a = new Regex(first);          //class Ststem.regularExpressions.Regex
+                                                 //Regex represent an immutable regular expression.
+
+            if (a.IsMatch(name))        //checking the condition with the help of if /else statement.
+                                        //IsMatch :indicates whether the regular expression specified in the Regex constructor finds a match in a specified input string.
+            {
+                Console.WriteLine(name + " this first name is valid .");  //if the conduction is true.
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine(name + " this first name is not valid, please type first word in uppercase. ");    //if condition is false.
+                Console.ReadLine();
+            }
+        }
+
+        public static void LastName(string name)         //declaring the static meythod to take user input.
+        {
+            string lastname = "^[A-Z]{1}[a-z]{2,}$";        //write regularExpration.   ^=start  $=end.
+            Regex a = new Regex(lastname);          //class Ststem.regularExpressions.Regex
+                                                    //Regex represent an immutable regular expression.
+
+            if (a.IsMatch(name))        //checking the condition with the help of if /else statement.
+                                        //IsMatch :indicates whether the regular expression specified in the Regex constructor finds a match in a specified input string.
+            {
+                Console.WriteLine(name + " this last name is valid .");  //if the conduction is true.
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine(name + " this last name is not valid ,please type first word in uppercase.");    //if condition is false.
+                Console.ReadLine();
+            }
+        }
+
+    }
+}
